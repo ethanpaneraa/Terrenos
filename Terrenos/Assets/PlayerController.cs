86a5e117ad71211d57ca760fa82688dc0a3c8c95
tecwit <antoniorocha2023@u.northwestern.edu>
2023-01-31 00:07:54 -0600

    Unity Project Setup + Moving Player

---
+++ Terrenos/Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 2;
+    public float jumpHeight = 4;
+    public bool onGround = true;
+    public float horizontalMovement;
+    public float verticalMovement;
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * movementSpeed;
+        verticalMovement = Input.GetAxisRaw("Jump");
+        if (onGround && verticalMovement > 0.1)
+        {
+            verticalMovement = jumpHeight;
+        }
+        else
+        {
+            verticalMovement = rigidBody.velocity.y;
+        }
+        rigidBody.velocity = new Vector2(horizontalMovement, verticalMovement);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ground"))
+        {
+            onGround = false;
+        }    
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ground"))
+        {
+            onGround = true;
+        }
+    }
+}
