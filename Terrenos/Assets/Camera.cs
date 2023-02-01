b8a4325aa8a5977b4196685813a9fe46b4ff10cb
tecwit <antoniorocha2023@u.northwestern.edu>
2023-02-01 00:18:10 -0600

    Camera Smoothing
    Camera follows player

---
+++ Terrenos/Assets/Camera.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float smoothing = 0.5f;
+    public Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Transform cameraTransform = GetComponent<Transform>();
+        var playerX = Mathf.Lerp(cameraTransform.position.x, playerTransform.position.x, smoothing);
+        var playerY = Mathf.Lerp(cameraTransform.position.y, playerTransform.position.y, smoothing);
+        Vector3 pos = new Vector3(playerX, playerY, cameraTransform.position.z);
+        cameraTransform.position = pos;
+    }
+}
