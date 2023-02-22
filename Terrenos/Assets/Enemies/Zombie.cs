99e66f348c7ff6c6954fd9d75cf4f1442ad0fb34
Ethan Pineda <joshuapineda66@gmail.com>
2023-02-22 01:38:40 -0600

    added enemy spawner

---
+++ Terrenos/Assets/Enemies/Zombie.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int zombieHealth = 100; 
+    public int zombieMovementSpeed = 2; 
+    public int zombieAttackDamage = 20; 
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Terrenos/Assets/EnemySpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Zombie Zombie; 
+    public Skeleton Skeleton; 
+
+    public int numEnemiesToSpawn = 5;  // The number of enemies to spawn at once
+    public float spawnInterval = 100f;  // The time interval between enemy spawns
+    private float timeSinceLastSpawn = 0f;  // The time elapsed since the last enemy spawn
+    private int numEnemiesSpawned = 0;  // The number of enemies spawned in the current wave
+
+    void Update()
+    {
+        // Increment the time since the last enemy spawn
+        timeSinceLastSpawn += Time.deltaTime;
+
+        // If the time since the last spawn is greater than or equal to the spawn interval...
+        if (timeSinceLastSpawn >= spawnInterval)
+        {
+            // Reset the time since the last spawn
+            timeSinceLastSpawn = 0f;
+            numEnemiesSpawned = 0;
+
+            // Spawn multiple instances of the enemy prefab at intervals
+            StartCoroutine(SpawnEnemiesInterval());
+        }
+    }
+
+    IEnumerator SpawnEnemiesInterval()
+    {
+        while (numEnemiesSpawned < numEnemiesToSpawn)
+        {
+            // Spawn an instance of the enemy prefab at the spawner's position and rotation
+            Instantiate(Zombie, transform.position, transform.rotation);
+
+            numEnemiesSpawned++;
+
+            // Wait for 1 second before spawning the next enemy
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
