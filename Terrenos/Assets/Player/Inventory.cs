d01e358f9cb7f85602d7a58e65dbbc3b03110108
tecwit <antoniorocha2023@u.northwestern.edu>
2023-02-26 16:40:56 -0600

    Sprite work
    - Sprites render into hand
    - Changed animations to "front" "back" to better support item rendering
    - Changed scene to include player component name changes.
    - Started to add components to control for item switching in hand

--- Terrenos/Assets/Player/Inventory.cs
+++ Terrenos/Assets/Player/Inventory.cs
@@ -9,16 +9,29 @@ public class Inventory : MonoBehaviour
     public Sprite swordSprite;
     public Sprite pickaxeSprite;
     public Sprite blockSprite;
+    private GameObject front_hand;
+    private GameObject Bow;
+    private GameObject Sword;
+    private GameObject Pickaxe;
+    private GameObject Block;
+    private GameObject activeItem;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Hand
+        front_hand = GameObject.Find("male_arm_front");
+
         // Bow
         InventoryItem bow = new InventoryItem();
         bow.Sprite = bowSprite;
         bow.HitDamage = 1;
         bow.itemName = "bow";
         InventoryItems.Add(bow);
+        Bow = new GameObject("Bow");
+        Bow.AddComponent<SpriteRenderer>();
+        Bow.GetComponent<SpriteRenderer>().sprite = bowSprite;
+        Bow.SetActive(false);
 
         // Sword
         InventoryItem sword = new InventoryItem();
@@ -26,6 +39,11 @@ public class Inventory : MonoBehaviour
         sword.HitDamage = 2;
         sword.itemName = "sword";
         InventoryItems.Add(sword);
+        Sword = new GameObject("Sword");
+        Sword.AddComponent<SpriteRenderer>();
+        Sword.GetComponent<SpriteRenderer>().sprite = swordSprite;
+        Sword.SetActive(false);
+
 
         // Pickaxe
         InventoryItem pickaxe = new InventoryItem();
@@ -34,6 +52,11 @@ public class Inventory : MonoBehaviour
         pickaxe.Destroys = true;
         pickaxe.itemName = "pickaxe";
         InventoryItems.Add(pickaxe);
+        Pickaxe = new GameObject("Pickaxe");
+        Pickaxe.AddComponent<SpriteRenderer>();
+        Pickaxe.GetComponent<SpriteRenderer>().sprite = pickaxeSprite;
+        Pickaxe.SetActive(true);
+        activeItem = Pickaxe;
 
         // Block
         InventoryItem block = new InventoryItem();
@@ -41,15 +64,20 @@ public class Inventory : MonoBehaviour
         block.Places = true;
         InventoryItems.Add(block);
         block.itemName = "block";
+        Block = new GameObject("Block");
+        Block.AddComponent<SpriteRenderer>();
+        Block.GetComponent<SpriteRenderer>().sprite = blockSprite;
+        Block.SetActive(false);
 
         // Empty
 
         // Etc
+        GameObject slot_0 = GameObject.Find("InventorySlot (0)");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        activeItem.transform.position = front_hand.transform.position;
     }
 }
