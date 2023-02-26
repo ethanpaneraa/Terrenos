fdbd55d12b3bc8e66d45756ac4c80bcc2859621c
tecwit <antoniorocha2023@u.northwestern.edu>
2023-02-26 16:47:32 -0600

    Player background limb changes

--- Terrenos/Assets/Player/Inventory.cs
+++ Terrenos/Assets/Player/Inventory.cs
@@ -14,7 +14,7 @@ public class Inventory : MonoBehaviour
     private GameObject Sword;
     private GameObject Pickaxe;
     private GameObject Block;
-    private GameObject activeItem;
+    public GameObject activeItem;
 
     // Start is called before the first frame update
     void Start()
